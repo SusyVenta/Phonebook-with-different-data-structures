55ab97d79600471c89c54b0e60d1ee6dd6d8d46e
Susy Ventafridda <ventafri@amazon.lu>
2019-04-16 22:12:34 +0200

    Added unit tests for address class

--- Singly_Linked_List/address.py
+++ Singly_Linked_List/address.py
@@ -1,22 +1,20 @@
 class Address:
-    def __init__(self, address="", address_type=""):
+    def __init__(self):
         self.addresses = {"Home": [], "Work": [], "Other": []}
 
-    def get_address(self, address_type, index=""):
-        if index != "":
-            return "\n".join(self.addresses[address_type])
-        else:
-            return self.addresses[address_type][int(index)]
+    def get_address(self, address_type="Home", index=0):
+        out_address = self.addresses[address_type][index]
+        return out_address
 
     def get_all_addresses(self):
         all_addresses = []
         for address_type, address_list in self.addresses.items():
             for address in address_list:
-                all_addresses.append("{} Address: {}".format(address_type, address))
-        return "\n".join(all_addresses)
+                all_addresses.append("{} Address: {}\n".format(address_type, address))
+        return "".join(all_addresses)
 
     def add_address(self, address, address_type="Home"):
         self.addresses[address_type].append(address)
 
-    def modify_address(self, address, address_type, index):
+    def modify_address(self, address, address_type="Home", index=0):
         self.addresses[address_type][index] = address
